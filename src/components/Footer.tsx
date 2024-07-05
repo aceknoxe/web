7beba2783381c5bf2c3fe61d19922744b97df60b
aceknoxe <harigovindes@outlook.com>
2024-07-05 13:01:12 +0530

    link tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,36 +8,52 @@ const Footer: React.FC = () => {
       <div className="max-w-7xl mx-auto px-8 md:px-10 lg:px-10">
         <div className="grid grid-cols-1 gap-10 mb-10 text-gray-400 lg:grid-cols-2 ">
           <div>
-            <h4 className="mb-6 font-semibold text-gray-900 uppercase text-center ">Company</h4>
+            <h4 className="mb-6 font-semibold text-gray-900 uppercase text-center">Company</h4>
             <ul className="space-y-3 flex flex-col items-center">
               <li>
-                <a href="/about" className="text-gray-500 hover:text-gray-700 ">About Us</a>
+                <Link href="/about">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">About Us</span>
+                </Link>
               </li>
               <li>
-                <a href="/gallery" className="text-gray-500 hover:text-gray-700">Gallery</a>
+                <Link href="/gallery">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Gallery</span>
+                </Link>
               </li>
               <li>
-                <a href="/member" className="text-gray-500 hover:text-gray-700">Execome</a>
+                <Link href="/member">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Execome</span>
+                </Link>
               </li>
               <li>
-                <a href="/blog" className="text-gray-500 hover:text-gray-700">Blog</a>
+                <Link href="/blog">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Blog</span>
+                </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h4 className="mb-6 font-semibold text-gray-900 uppercase text-center ">Resources</h4>
+            <h4 className="mb-6 font-semibold text-gray-900 uppercase text-center">Resources</h4>
             <ul className="space-y-3 flex flex-col items-center">
               <li>
-                <a href="/arduino" className="text-gray-500 hover:text-gray-700">Arduino</a>
+                <Link href="/arduino">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Arduino</span>
+                </Link>
               </li>
               <li>
-                <a href="/raspberry" className="text-gray-500 hover:text-gray-700">Raspberry</a>
+                <Link href="/raspberry">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Raspberry</span>
+                </Link>
               </li>
               <li>
-                <a href="/embedded-c" className="text-gray-500 hover:text-gray-700">Embedded C</a>
+                <Link href="/embedded-c">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">Embedded C</span>
+                </Link>
               </li>
               <li>
-                <a href="/micropython" className="text-gray-500 hover:text-gray-700">MicroPython</a>
+                <Link href="/micropython">
+                  <span className="text-gray-500 hover:text-gray-700 cursor-pointer">MicroPython</span>
+                </Link>
               </li>
             </ul>
           </div>
@@ -45,12 +61,14 @@ const Footer: React.FC = () => {
         <div className="flex justify-center py-7">
           <SocialLinks />
         </div>
-        <div className="text-center text-gray-700 mt-10 ">
+        <div className="text-center text-gray-700 mt-10">
           <p className="text tex-semibold">
             Made by{' '}
             <a
               href="https://instagram.com"
               className="underline text-green-300 hover:text-green-500"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               me
             </a>

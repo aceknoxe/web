0589b648c2022ebc2efe34b26478b203b862ff7c
aceknoxe <harigovindes@outlook.com>
2024-07-13 18:07:11 +0530

    last min fixes

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,108 +1,128 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Photos: React.FC = () => {
   return (
     <div className="py-20 bg-opacity-50 bg-emerald-50"> 
-    <div className="max-w-3xl px-8 mx-auto sm:px-6 lg:max-w-5xl lg:px-8 mt-8">
-    <div className="inline-block px-3 py-1 mt-12 text-sm rounded-lg md:md-0 bg-emerald-100 bg-opacity-60 text-emerald-500">
-      Product in Reaction
+      <div className="max-w-3xl px-8 mx-auto sm:px-6 lg:max-w-5xl lg:px-8 mt-8">
+        <div className="inline-block px-3 py-1 mt-12 text-sm rounded-lg md:md-0 bg-emerald-100 bg-opacity-60 text-emerald-500">
+          Product in Reaction
+        </div>
+        <h2 className="mt-4 bg-gradient-to-r from-[#1D976C] to-[#38ef7d] bg-clip-text text-3xl font-medium text-transparent">
+          Implementation In Favor
+        </h2>
+        <p className='mt-4 text-lg font-normal text-gray-700 md:text-center'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aperiam doloremque error, placeat distinctio commodi 
+        </p>
+        <div className="mt-6 grid grid-cols-2 gap-4 md:grid-cols-4 px-2">
+          <div className="grid gap-2">
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image4.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-40">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image1.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <div className="relative group h-40">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image2.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image1.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-40">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image2.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image4.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-40">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image3.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image3.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <div className="relative group h-40">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image4.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+            <div className="relative group h-60">
+              <Image
+                className="rounded-lg object-cover"
+                src="/photos/image.png"
+                alt="gallery-photo"
+                layout="fill"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <h2 className="mt-4 bg-gradient-to-r from-[#1D976C] to-[#38ef7d] bg-clip-text text-3xl font-medium text-transparent">
-    Implementation In Favor
-    </h2>
-    <p className='mt-4 text-lg font-normal  text-gray-700 md:text-center'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aperiam doloremque error, placeat distinctio commodi 
-    </p>
-    <div className="mt-6 grid grid-cols-2 gap-4 md:grid-cols-4 px-2 ">
-      <div className="grid gap-4">
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://docs.material-tailwind.com/img/team-3.jpg"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://docs.material-tailwind.com/img/team-3.jpg"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div className="relative group">
-          <img
-            className="h-auto max-w-full rounded-lg object-cover object-center transition duration-300 transform group-hover:scale-105"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-);
+  );
 }
 
 export default Photos;
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -2,13 +2,37 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import blogs from '../../public/blogs/blogs.json';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Blogs = () => {
+// Define the Blog interface
+interface Blog {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  date: string;
+  image: string;
+}
+
+// Define the props type for the Blogs component
+interface BlogsProps {
+  blogs: Blog[];
+}
+
+// Fetch the blogs data on the server side
+export const getServerSideProps = async () => {
+  const blogs: Blog[] = require('../../public/blogs/blogs.json');
+  return {
+    props: {
+      blogs,
+    },
+  };
+};
+
+const Blogs = ({ blogs }: BlogsProps) => {
   const router = useRouter();
-  const [selectedBlog, setSelectedBlog] = useState(null);
+  const [selectedBlog, setSelectedBlog] = useState<Blog | null>(null);
 
   // Reset selectedBlog state when navigating back to top
   useEffect(() => {
@@ -21,7 +45,7 @@ const Blogs = () => {
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, []);
+  }, [router.events]);
 
   return (
     <>
@@ -39,7 +63,7 @@ const Blogs = () => {
         <main className="flex-grow mt-8 container mx-auto px-4 py-8">
           <h1 className="mt-16 py-7 bg-gradient-to-r from-[#1D976C] to-[#38ef7d] bg-clip-text text-3xl font-medium text-transparent text-center">Latest Blog Posts</h1>
           <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {blogs.map((blog) => (
+            {blogs.map((blog: Blog) => (
               <Link key={blog.id} href={`/blog/${blog.id}`} passHref>
                 <div className="bg-opacity-50 bg-emerald-50 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 cursor-pointer">
                   <img
